e57b55e19fbbaab2c0511f609f1b0ae2726e5177
Selahattin Güngör <67766746+SelahattinGngr@users.noreply.github.com>
2022-01-29 17:02:30 +0300

    Add files via upload

---
+++ Dizi Kuralları/array_change_key_case CASE_LOWER CASE_UPPER/array_change_key_case.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="tr-TR">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title>Php Denemeleri</title>
+</head>
+
+<body>
+  <?php
+/*
+array_change_key_case()  : Dizi içerisinde bulunan anahtar veya anahtarların büyük küçük harf 
+dönüştürme işlmi için yapılır.
+CASE_LOWER : küçük harf yapar.
+CASE_UPPER : Büyük harf yapar.
+*/
+
+$Isimler = array("IsimBir" => "Selo", "IsimIki" => "can", "IsimUc" => "Hakkı", "IsimDort" => "apo", "IsimBes" => "wow");
+
+echo "<pre>";
+print_r($Isimler);
+echo "</pre><br />";
+
+$Sonuc =  array_change_key_case($Isimler);
+
+echo "<pre>";
+print_r($Sonuc);
+echo "</pre>";
+
+?>
+</body>
+</html
